417f1b46f06e134d100bacc7b90bb585f40dd561
TatjanaGerasimenko <t.v.gerasimenko@yandex.ru>
2021-07-18 09:21:17 -0700

    gitignore + packages

--- mysite/settings.py
+++ mysite/settings.py
@@ -41,9 +41,10 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
+
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
